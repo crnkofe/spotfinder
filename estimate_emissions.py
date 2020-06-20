40530beef9ff61b889c8cae2e2475019038f7d8c
Simon Mihevc <simon.mihevc@gmail.com>
2020-06-20 16:47:27 +0200

    Simplistic estimate of CO2 per year consumption

--- estimate_emissions.py
+++ estimate_emissions.py
@@ -10,6 +10,10 @@
 
 import json
 import random
+from matplotlib import pyplot as plt
+import numpy as np
+import math #needed for definition of pi
+
 
 emission_limits = {
     # EURO 6 (Petrol)
@@ -57,7 +61,7 @@ car_co2_emmissions = {
 # standard selection
 standard = "euro_6"
 # car selection
-cars = "opel_corsa_14"
+car = "opel_corsa_14"
 # simulate count days
 count_days = 356
 # count cars for which to simulate
@@ -81,13 +85,55 @@ def simulate_laps(prob, max_laps):
     return max_laps
 
 
+def calculate_emissions(distance, emission_limits, co2_limit):
+    return {
+        "CO_total": distance * emission_limits.get("CO", 0), # g
+        "THC_total": distance * emission_limits.get("THC", 0),  # g
+        "NMHC_total": distance * emission_limits.get("NMHC", 0), # g
+        "NOx_total": distance * emission_limits.get("NOx", 0),
+        "CO2_total": distance * co2_limit
+    }
+
+
+minimal_emissions_per_day = []
+emissions_per_day = []
+
 total_laps = 0
 total_lap_length = 0
 for day in range(count_days):
     total_laps += simulate_laps(bullseye_probability, cutoff)
-
+    current_lap_length = total_laps * lap_length
+    # for minimal emissions assume driver takes at most one lap
+    # to find a free parking spot using the spotfinder
+    minimal_emissions_per_day.append(calculate_emissions(
+        day * lap_length,
+        emission_limits.get(standard, {}),
+        car_co2_emmissions.get(car, 0)))
+    emissions_per_day.append(calculate_emissions(
+        current_lap_length,
+        emission_limits.get(standard, {}),
+        car_co2_emmissions.get(car, 0)))
 
 total_lap_length = total_laps * lap_length
+
 print("Total laps: {}".format(total_laps))
 print("Distance driven: {ln:.2f} km".format(ln=total_lap_length))
+total_emissions = json.dumps(
+    calculate_emissions(total_lap_length,
+                        emission_limits.get(standard, {}),
+                        car_co2_emmissions.get(car, 0)),
+    )
+print("Total emissions: {}".format(total_emissions))
 
+x = np.arange(0, count_days, 1)
+y = np.array(list(map(lambda x: x.get("CO2_total", 0) / 1e3, emissions_per_day)))
+plt.plot(x, y, label="CO2 no-action estimate")
+
+y_min = np.array(list(map(lambda x: x.get("CO2_total", 0) / 1e3, minimal_emissions_per_day)))
+plt.plot(x, y_min, label="CO2 spotfinder estimate")
+
+plt.xlabel("day")
+plt.ylabel("CO2 [kg]")
+
+plt.title('CO2 emissions savings estimate')
+plt.show()
